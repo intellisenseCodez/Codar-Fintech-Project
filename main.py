05259703d01e7f84fbcaba5f29a05952a9af04c7
Abisola <abisolaonileimo@yahoo.com>
2024-01-31 14:12:43 +0100

    pull from master

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 import fintech
 
 print("ANALYSIS QUESTION")
 print(f"1. The Total number of Users is {fintech.count_users()}")
-=======
-import os
-
-
-file_path = "Codar-Fintech-Project/users_data.json"
-
-with open(file_path, 'r') as user_json:
-    data = user_json.read()
-    print(len(data))
->>>>>>> Abisola-branch
