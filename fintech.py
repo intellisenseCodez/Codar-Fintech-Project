e1462d558497104bc7568c52d5e106c94c7c5375
Abisola <abisolaonileimo@yahoo.com>
2024-01-31 12:02:44 +0100

    my changes

---
+++ fintech.py
@@ -0,0 +1,3 @@
+file_path = "Codar-Fintech-Project/users_data.json"
+file_path = "Codar-Fintech-Project/cards_data.json"
+def 
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Abisola")
+import os
+
+
+file_path = "Codar-Fintech-Project/users_data.json"
+
+with open(file_path, 'r') as user_json:
+    data = user_json.read()
+    print(len(data))
