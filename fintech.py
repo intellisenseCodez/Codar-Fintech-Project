1d9959235d78ebbabdfdfb81710ea0f1ccd381f8
Oyekanmi lekan <oyekanmiakande@gmail.com>
2024-01-31 12:00:45 +0100

    calculate the total number of users

---
+++ fintech.py
@@ -0,0 +1,21 @@
+
+file_path = "fintech-data/users_data.json"
+file_path = "fintech-data/cards_data.json"
+
+# 1. The Total number of Users (Master)
+def count_users():
+    """count the numbers of user details in the file"""
+    with open(file_path, 'r') as user_json:
+            data = user_json.read()
+            
+    return len(data)
+
+# 2. The Total number of Unique Users (Abisola)
+
+# 3. Average Score of all users (Pholasade)
+
+# 4. The Total number of Cards (Emmanuel)
+
+# 5. The Total number of Unique Cards (Lanre)
+
+
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Abisola")
+import fintech
+
+print("ANALYSIS QUESTION")
+print(f"1. The Total number of Users is {fintech.count_users()}")
